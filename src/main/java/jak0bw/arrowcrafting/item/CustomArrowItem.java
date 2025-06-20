07099e51aeaa909b58d7bfb1bc6af39b4c15ca2d
JakobPCoder <j4712@web.de>
2025-06-20 20:53:12 +0200

    Added tooltips

--- src/main/java/jak0bw/arrowcrafting/item/CustomArrowItem.java
+++ src/main/java/jak0bw/arrowcrafting/item/CustomArrowItem.java
@@ -7,9 +7,15 @@ import net.minecraft.entity.projectile.ProjectileEntity;
 import net.minecraft.item.ArrowItem;
 import net.minecraft.item.Item;
 import net.minecraft.item.ItemStack;
+import net.minecraft.item.tooltip.TooltipType;
+import net.minecraft.util.Formatting;
+import net.minecraft.text.Text;
 import net.minecraft.util.math.Direction;
 import net.minecraft.util.math.Position;
 import net.minecraft.world.World;
+
+import java.util.List;
+
 import org.jetbrains.annotations.Nullable;
 
 
@@ -40,5 +46,11 @@ public class CustomArrowItem extends ArrowItem {
         CustomArrowEntity arrowEntity = new CustomArrowEntity(this.arrowDamageMultiplier, world, pos.getX(), pos.getY(), pos.getZ(), stack.copyWithCount(1), null);
         arrowEntity.pickupType = PersistentProjectileEntity.PickupPermission.ALLOWED;
         return arrowEntity;
+    }
+    
+    @Override
+    public void appendTooltip(ItemStack stack, TooltipContext context, List<Text> tooltip, TooltipType type) {
+        tooltip.add(Text.translatable("item.arrowcrafting.arrow.damage", (100 * this.arrowDamageMultiplier) + "%")
+            .formatted(Formatting.BLUE, Formatting.ITALIC));
     }
 } 
